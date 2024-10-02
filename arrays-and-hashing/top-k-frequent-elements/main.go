a7e9307c6db69fb5331cc9af2c41ce389a8ca1d8
Maciej Sieradz <macieks53@gmail.com>
2024-10-02 15:14:00 +0200

    Solution

--- arrays-and-hashing/top-k-frequent-elements/main.go
+++ arrays-and-hashing/top-k-frequent-elements/main.go
@@ -10,6 +10,36 @@ func main() {
 }
 
 func topKFrequent(nums []int, k int) []int {
+	count := make(map[int]int)
+	freq := make([][]int, len(nums)+1)
+
+	for _, n := range nums {
+		count[n] = 1 + count[n]
+	}
+
+	for key, value := range count {
+		fmt.Printf("key: %d, value: %d\n", key, value)
+	}
+
+	for n, c := range count {
+		freq[c] = append(freq[c], n)
+	}
+
+	var res []int
+
+	for i := len(freq) - 1; i > 0; i-- {
+		for _, n := range freq[i] {
+			res = append(res, n)
+			if len(res) == k {
+				return res
+			}
+		}
+	}
+
+	return []int{}
+}
+
+func topKFrequentSlow(nums []int, k int) []int {
 	numbers := make(map[int]int)
 
 	for _, number := range nums {
