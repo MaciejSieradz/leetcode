7d98e4dc60d2e7675b00d6e49497a85957b6e9dd
Maciej Sieradz <macieks53@gmail.com>
2024-10-02 14:54:57 +0200

    Solution

---
+++ arrays-and-hashing/top-k-frequent-elements/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Print(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
+}
+
+func topKFrequent(nums []int, k int) []int {
+	numbers := make(map[int]int)
+
+	for _, number := range nums {
+		num, ok := numbers[number]
+
+		if !ok {
+			numbers[number] = 1
+		} else {
+			numbers[number] = num + 1
+		}
+	}
+
+	keys := make([]int, 0, len(numbers))
+
+	for key := range numbers {
+		keys = append(keys, key)
+	}
+
+	for key, value := range numbers {
+		fmt.Printf("key: %d, value: %d\n", key, value)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return numbers[keys[i]] > numbers[keys[j]]
+	})
+
+	return keys[:k]
+}
